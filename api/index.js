a3e2a17fe81711efc70dc0601a1b0fa1d218633b
Shani757 <zeeshanhasnain785@gmail.com>
2023-11-13 17:01:42 +0500

    upto signup page functionality

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO_CONNECTION_STRING)
+    .then(() => console.log("MongoDB connected!"))
+    .catch(() => console.log("Error connectiig your DB!"));
+};
 
-const app = express()
+const app = express();
+app.use(express.json());
+connect();
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}!`);
+});
 
-app.listen(3000, ()=>{
-    console.log('Server running on port 3000!')
-})
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
